1b1a6b695400c24cbffc64f40ff4d7cd7fffdba9
Asrar hussain <asra.hussain@digital.justice.gov.uk>
2024-11-14 12:17:47 +0000

    changing owner of clamav directory in the script

--- scripts/wrapper-entrypoint.sh
+++ scripts/wrapper-entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 ls -ld /var/lib/clamav
+chown -R clamav:clamav /var/lib/clamav
 # Replace the PrivateMirror URL in the freshclam.conf file
 sed -i 's|PrivateMirror .*|PrivateMirror '"$MIRROR_URL"'|g' /etc/clamav/freshclam.conf
 
