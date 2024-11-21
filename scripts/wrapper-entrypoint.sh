8830ed578de10038150bd8aa705c434352292e93
Asrar hussain <asra.hussain@digital.justice.gov.uk>
2024-11-21 13:59:07 +0000

    simplyfying the freschlam switch logic

--- scripts/wrapper-entrypoint.sh
+++ scripts/wrapper-entrypoint.sh
@@ -4,11 +4,9 @@ ls -ld /var/lib/clamav
 # Check if MIRROR_URL is defined and not empty
 if [ -n "$MIRROR_URL" ]
 then
-    # MIRROR_URL is defined, replace all the PrivateMirror URLs in the freshclam.conf file
-    sed -i 's|PrivateMirror .*|PrivateMirror '"$MIRROR_URL"'|g' /etc/clamav/freshclam.conf
-else
-    # MIRROR_URL is not defined, comment out all the PrivateMirror lines
-    sed -i 's|^PrivateMirror|##&|' /etc/clamav/freshclam.conf
+    # MIRROR_URL is defined
+    # Uncomment the PrivateMirror line and replace mirror1.example.com with the provided MIRROR_URL
+    sed -i 's|#PrivateMirror mirror1.example.com|PrivateMirror '"$MIRROR_URL"'|g' /etc/clamav/freshclam.conf
 fi
 
 # Execute the original entrypoint script
