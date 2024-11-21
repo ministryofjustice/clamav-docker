ba200a11ac1d95abc16b538abba88560f2493524
Asrar hussain <asra.hussain@digital.justice.gov.uk>
2024-11-21 13:40:57 +0000

    Fixing sed command to comment out private mirrror if not being used

--- scripts/wrapper-entrypoint.sh
+++ scripts/wrapper-entrypoint.sh
@@ -4,11 +4,11 @@ ls -ld /var/lib/clamav
 # Check if MIRROR_URL is defined and not empty
 if [ -n "$MIRROR_URL" ]
 then
-    # MIRROR_URL is defined, replace the PrivateMirror URL in the freshclam.conf file
+    # MIRROR_URL is defined, replace all the PrivateMirror URLs in the freshclam.conf file
     sed -i 's|PrivateMirror .*|PrivateMirror '"$MIRROR_URL"'|g' /etc/clamav/freshclam.conf
 else
-    # MIRROR_URL is not defined, comment out the PrivateMirror line
-    sed -i 's|^PrivateMirror|## PrivateMirror|g' /etc/clamav/freshclam.conf
+    # MIRROR_URL is not defined, comment out all the PrivateMirror lines
+    sed -i 's|^PrivateMirror|##&|' /etc/clamav/freshclam.conf
 fi
 
 # Execute the original entrypoint script
