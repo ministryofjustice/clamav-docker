d707e7045bd2b8ed128feb7e13319ead1299dff2
Asrar hussain <asra.hussain@digital.justice.gov.uk>
2024-11-21 13:30:15 +0000

    Addig in a mirror check If a mirror url is provided it will setup on a private mirror otherwise it will default to the central clam mirror

--- scripts/wrapper-entrypoint.sh
+++ scripts/wrapper-entrypoint.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 ls -ld /var/lib/clamav
-# Replace the PrivateMirror URL in the freshclam.conf file
-sed -i 's|PrivateMirror .*|PrivateMirror '"$MIRROR_URL"'|g' /etc/clamav/freshclam.conf
+
+# Check if MIRROR_URL is defined and not empty
+if [ -n "$MIRROR_URL" ]
+then
+    # MIRROR_URL is defined, replace the PrivateMirror URL in the freshclam.conf file
+    sed -i 's|PrivateMirror .*|PrivateMirror '"$MIRROR_URL"'|g' /etc/clamav/freshclam.conf
+else
+    # MIRROR_URL is not defined, comment out the PrivateMirror line
+    sed -i 's|^PrivateMirror|## PrivateMirror|g' /etc/clamav/freshclam.conf
+fi
 
 # Execute the original entrypoint script
 exec /init
